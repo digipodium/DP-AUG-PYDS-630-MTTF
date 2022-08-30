ee271843b5387e78a1f781dc4486d7c5308dd690
zaidkamil <xaidmetamorphos@gmail.com>
2022-08-30 20:05:14 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+x = 91203
+y = -9031293
+z = 569090495045904950490549095
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+x = 1.10
+y = 2.20
+z = .232
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
