f10b98da95faaccd932bb755614e4fe88ccd4cd4
zaidkamil <xaidmetamorphos@gmail.com>
2022-09-30 20:03:14 +0530

    lambda and other func

---
+++ fun5.py
@@ -0,0 +1,12 @@
+
+from pytest import mark
+
+
+def marks(**data):
+    with open('marks.txt','a') as f:
+        for n,m in data.items():
+            f.write(f'{n}:{m}\n')
+    
+marks(rajesh=200, brijesh=120)
+marks(raj=130, ajay=150, suraj=90, chand=120)
+marks()
